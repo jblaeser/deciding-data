0bd47c72e9f226a1467b86125151905e6337faab
Jill Blaeser <jblaeser01@gmail.com>
2019-02-24 13:32:01 -0600

    Churchill info

--- script.js
+++ script.js
@@ -51,6 +51,18 @@ document.getElementById('BtnDonate').addEventListener('click', function(){
 document.getElementById('BtnChurchill').addEventListener('click', function(){
   //Code in here executes when the user clicks the "Churchill" button.
   console.log("This speech was written by " + churchillSpeech.author + " in " + churchillSpeech.year + ".");
+
+  if (churchillSpeech.yearIsBCE === true) {
+    console.log("This speech took place before the common era.")
+  } else {console.log("This speech took place during the common era.")};
+
+
+  if (churchillSpeech.year > ghandiSpeech.year && churchillSpeech.year > demosthenesSpeech.year) {
+    console.log("This is the most recent speech on the page!")
+  } else if (churchillSpeech.year < ghandiSpeech.year && churchillSpeech.year < demosthenesSpeech.year) {
+    console.log("This is the oldest speech on the page!")
+  } else {
+    console.log("This speech is neither the oldest or newest speech on the page. Juuuust right, Goldilocks")};
 });
 
 document.getElementById('BtnGhandi').addEventListener('click', function(){
